4b4c400fe0f5ad07fdf442284b0920e4d7e57f8d
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2021-07-26 16:01:03 -0500

    modified code to work with new design

--- js/calculator.js
+++ js/calculator.js
@@ -1,18 +1,14 @@
 export function calculator() {
     const calculationsDiv = document.getElementById("calculations");
 
-    const startDateText = document.getElementById("startDate");
-    const currentMultiplier = document.getElementById("currentMultiplier");
+    const startDateElement = document.getElementById("startDateElement");
+    const currentMultiplierElement = document.getElementById("currentMultiplierElement");
 
-    const expectedHeadingText = document.getElementById("expectedHeading");
-    const expectedDateText = document.getElementById("expectedDate");
-    const expectedMultiplier = document.getElementById("expectedMultiplier");
-    //const expectedXP = document.getElementById("expectedXP");
+    const desiredHeading = document.getElementById("desiredHeading");
+    const expectedElement = document.getElementById("expectedElement");
+    const expectedMultiplierElement = document.getElementById("expectedMultiplierElement");
 
-    const info = document.getElementById("info");
-
-    const streakToDateRadio = document.getElementById("streakToDate");
-    const dateToStreakRadio = document.getElementById("dateToStreak");
+    const infoElement = document.getElementById("infoElement");
 
     const desiredStreakTextFieldDiv = document.getElementById("desiredStreakTextFieldDiv");
     const desiredDateInputDiv = document.getElementById("desiredDateInputDiv");
@@ -20,16 +16,17 @@ export function calculator() {
     const desiredStreakTextField = document.getElementById("desiredStreakTextField");
     const desiredDateInput = document.getElementById("desiredDateInput");
     const currentStreakTextField = document.getElementById("currentStreakTextField");
-    //const currentLevelTextField = document.getElementById("currentLevelTextField");
 
     const yesRadio = document.getElementById("yesRadio");
     const noRadio = document.getElementById("noRadio");
 
     const calculateButton = document.getElementById("calculateButton");
 
+    const changeCalculatorButton = document.getElementById("changeCalculatorButton");
+    const currentCalculatorElement = document.getElementById("currentCalculatorElement");
+
     calculateButton.addEventListener("click", calculate);
-    streakToDateRadio.addEventListener("input", changeCalculator);
-    dateToStreakRadio.addEventListener("input", changeCalculator);
+    changeCalculatorButton.addEventListener("click", changeCalculator);
 
     document.querySelectorAll("input[type='text'], input[type='date']").forEach(function(input) {
         input.addEventListener("keydown", function(event) {
@@ -37,15 +34,27 @@ export function calculator() {
         });
     });
 
-    changeCalculator();
+    let streakToDate = true;
 
     function changeCalculator() {
-        if (streakToDateRadio.checked) {
+        streakToDate = !streakToDate;
+
+        updateCalculator();
+    }
+
+    updateCalculator();
+
+    function updateCalculator() {
+        if (streakToDate) {
             desiredDateInputDiv.style.display = "none";
             desiredStreakTextFieldDiv.style.display = "block";
+            desiredHeading.textContent = "Desired Streak";
+            currentCalculatorElement.innerHTML = "<span class='gray'>Current Calculator:</span> Streak to Date";
         } else {
             desiredStreakTextFieldDiv.style.display = "none";
             desiredDateInputDiv.style.display = "block";
+            desiredHeading.textContent = "Desired Date";
+            currentCalculatorElement.innerHTML = "<span class='gray'>Current Calculator:</span> Date to Streak";
         }
     }
 
@@ -56,23 +65,23 @@ export function calculator() {
         function showInfo(show, text) {
             if (show) {
                 calculationsDiv.style.display = "none";
-                info.style.display = "block";
-                info.textContent = text;
+                infoElement.style.display = "block";
+                infoElement.textContent = text;
             } else {
                 calculationsDiv.style.display = "flex";
-                info.style.display = "none";
+                infoElement.style.display = "none";
             }
         }
 
-        if (currentStreakTextField.value == "" || /*currentLevelTextField.value == "" ||*/ !yesRadio.checked && !noRadio.checked) {
+        if (currentStreakTextField.value == "" || !yesRadio.checked && !noRadio.checked) {
             showInfo(true, enterFields);
-        } else if (streakToDateRadio.checked && desiredStreakTextField.value == "") {
+        } else if (streakToDate && desiredStreakTextField.value == "") {
             showInfo(true, enterFields);
-        } else if (dateToStreakRadio.checked && desiredDateInput.value == "") {
+        } else if (!streakToDate && desiredDateInput.value == "") {
             showInfo(true, enterFields);
-        } else if (isNaN(currentStreakTextField.value)/* || isNaN(currentLevelTextField.value)*/) {
+        } else if (isNaN(currentStreakTextField.value)) {
             showInfo(true, invalidNumber);
-        } else if (streakToDateRadio.checked && isNaN(desiredStreakTextField.value)) {
+        } else if (streakToDate && isNaN(desiredStreakTextField.value)) {
             showInfo(true, invalidNumber);
         } else {
             showInfo(false);
@@ -121,19 +130,19 @@ export function calculator() {
             const desiredDate = new Date(desiredDateParts[0], desiredDateParts[1] - 1, desiredDateParts[2]);
             const desiredDateStreak = Math.ceil((desiredDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));
 
-            startDateText.textContent = `Started on ${weekDays[startDate.getDay()]}, ${months[startDate.getMonth()]} ${numExtender(startDate.getDate())}, ${startDate.getFullYear()} (${startDaysAgo} days ago)`;
-            currentMultiplier.innerHTML = `<span>Multiplier:</span> x${(1 + (currentStreakTextField.value / 150)).toFixed(3)}`;
-            
-            //expectedXP.innerHTML = `<span>Estimated XP:</span> 500 XP`;
+            startDateElement.innerHTML = `<span class="gray">Started On:</span> ${weekDays[startDate.getDay()]}, ${months[startDate.getMonth()]} ${numExtender(startDate.getDate())}, 
+                ${startDate.getFullYear()} (${startDaysAgo} days ago)`;
 
-            if (streakToDateRadio.checked) {
-                expectedHeadingText.textContent = "Desired Streak";
-                expectedDateText.textContent = `Expected on ${weekDays[expectedDate.getDay()]}, ${months[expectedDate.getMonth()]} ${numExtender(expectedDate.getDate())}, ${expectedDate.getFullYear()} (${expectedDaysLeft} days left)`;
-                expectedMultiplier.innerHTML = `<span>Multiplier:</span> x${(1 + (desiredStreakTextField.value / 150)).toFixed(3)}`;
+            currentMultiplierElement.innerHTML = `<span class="gray">Multiplier:</span> x${(1 + (currentStreakTextField.value / 150)).toFixed(3)}`;
+
+            if (streakToDate) {
+                expectedElement.innerHTML = `<span class="gray">Expected On:</span> ${weekDays[expectedDate.getDay()]}, ${months[expectedDate.getMonth()]} ${numExtender(expectedDate.getDate())}, 
+                    ${expectedDate.getFullYear()} (${expectedDaysLeft} days left)`;
+
+                expectedMultiplierElement.innerHTML = `<span class="gray">Multiplier:</span> x${(1 + (desiredStreakTextField.value / 150)).toFixed(3)}`;
             } else {
-                expectedHeadingText.textContent = "Desired Date";
-                expectedDateText.innerHTML = `<span>Expected Streak:</span> ${desiredDateStreak}`;
-                expectedMultiplier.innerHTML = `<span>Multiplier:</span> x${(1 + (desiredDateStreak / 150)).toFixed(3)}`;
+                expectedElement.innerHTML = `<span class="gray">Expected Streak:</span> ${desiredDateStreak}`;
+                expectedMultiplierElement.innerHTML = `<span class="gray">Multiplier:</span> x${(1 + (desiredDateStreak / 150)).toFixed(3)}`;
             }
         }
     }
