0051b4b05e6fd3b0bf22360b55d81885109b2d4e
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-08-23 19:58:09 -0500

    remove commented code

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -41,49 +41,5 @@ export default function Switch({ checkedIcon, uncheckedIcon, className, ...props
                 </div>
             </button>
         </>
-        
     );
 }
-
-// import type { HTMLAttributes, ReactNode } from 'react';
-// import { useState, useRef, useId } from 'react';
-
-// interface SwitchProps extends HTMLAttributes<HTMLInputElement> {
-//     checkedIcon?: ReactNode,
-//     uncheckedIcon?: ReactNode
-// }
-
-// export default function Switch({ checkedIcon, uncheckedIcon, className, ...props }: SwitchProps) {
-//     const [checkboxChecked, setCheckboxChecked] = useState<boolean>(props.defaultChecked || false);
-//     const inputRef = useRef<HTMLInputElement>(null);
-//     const id = useId();
-
-//     function handleClick() {
-//         if (!inputRef.current) return;
-//         inputRef.current.click();
-//         setCheckboxChecked(inputRef.current.checked);
-//     }
-
-//     return (
-//         <div 
-//             className={`switch ${className ? className : ""}`}
-//             data-checked={checkboxChecked} 
-//             aria-role="checkbox"
-//             aria-checked={checkboxChecked}
-//             aria-controls={id}
-//             onClick={handleClick}
-//         >
-//             <div className="switch-track" />
-//             <div className="switch-thumb">
-//                 { checkboxChecked ? <>{checkedIcon}</> : <>{uncheckedIcon}</> }
-//             </div>
-
-//             <input 
-//                 type="checkbox" 
-//                 ref={inputRef}
-//                 id={id}
-//                 { ...props }
-//             />
-//         </div>
-//     );
-// }
